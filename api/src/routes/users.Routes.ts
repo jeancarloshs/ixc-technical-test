51c29ec8b48164fd31c3ad37b44dd4decbae5ea3
Jean <jeancarlos.na@hotmail.com>
2024-06-16 08:27:50 -0300

    hotfix: alterado nome das rotas de usuario

--- api/src/routes/users.Routes.ts
+++ api/src/routes/users.Routes.ts
@@ -4,7 +4,7 @@ import UserController from '../controllers/userController';
 const router = express.Router();
 
 router
-  .post('/api/v1/user_login', UserController.userLogin)
-  .post('/api/v1/user_create', UserController.userRegister)
+  .post('/api/v1/user/login', UserController.userLogin)
+  .post('/api/v1/user/create', UserController.userRegister)
 
 export default router;
