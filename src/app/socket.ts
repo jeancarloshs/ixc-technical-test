edf71b93da2d14b9f8c25c341325dd527cea3788
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2024-06-21 16:13:14 -0300

    hotfix: url socket

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-// const URL: any = process.env.NEXT_PUBLIC_URL_API;
-const URL = "http://127.0.0.1:3001/"
+const URL: any = process.env.NEXT_PUBLIC_URL_API;
+// const URL = "http://127.0.0.1:3001/"
 // console.log('URL', URL)
 
 // export const socket = io(URL, {
